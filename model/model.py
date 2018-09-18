1554a5eeb00e96d21e46e0dedf64931b988256c9
Irynei Baran <ibar@softserveinc.com>
2018-09-18 11:29:26 +0300

    Remove BaseModel class. Add support of torchvision models

--- model/model.py
+++ model/model.py
@@ -1,11 +1,14 @@
-from base import BaseModel
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import *
 
 
 def get_model_instance(model_name, **model_params):
     """
-    Get model from config file
+    Get model from config file.
+    Supports models from torchvision module.
+    Example:
+        get_model_instance('resnet101', num_classes=10)
     """
     try:
         model = eval(model_name)
@@ -17,15 +20,15 @@ def get_model_instance(model_name, **model_params):
     return model_instance
 
 
-class MnistModel(BaseModel):
-    def __init__(self, output_size):
+class MnistModel(nn.Module):
+    def __init__(self, num_classes):
         super(MnistModel, self).__init__()
-        self.output_size = output_size
+        self.num_classes = num_classes
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, self.output_size)
+        self.fc2 = nn.Linear(50, self.num_classes)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -35,31 +38,3 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return x
-
-
-class VGG16(BaseModel):
-    def __init__(self, output_size):
-        super(VGG16, self).__init__()
-        self.layers = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
-        self.features = self._make_layers(self.layers)
-        self.classifier = nn.Linear(512, output_size)
-
-    def forward(self, x):
-        out = self.features(x)
-        out = out.view(out.size(0), -1)
-        out = self.classifier(out)
-        return out
-
-    def _make_layers(self, cfg):
-        layers = []
-        in_channels = 3
-        for x in cfg:
-            if x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else:
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
-                in_channels = x
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-        return nn.Sequential(*layers)
