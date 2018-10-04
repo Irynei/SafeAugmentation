6a0b3f19424d95c5067cdf739e1f42cd52854ca9
Irynei Baran <i.baran@ucu.edu.ua>
2018-10-04 12:45:52 +0300

    Add early stopping logic

--- utils/util.py
+++ utils/util.py
@@ -14,3 +14,46 @@ def log_model_summary(model, verbose=False):
         params = sum([np.prod(p.size()) for p in model_parameters])
         log.info('Trainable parameters: {}'.format(params))
     log.info(model)
+
+
+class EarlyStopping:
+    def __init__(self, mode='min', min_delta=0, patience=10):
+        self.mode = mode
+        self.min_delta = min_delta
+        self.patience = patience
+        self.best = None
+        self.num_bad_epochs = 0
+        self.is_better = None
+        self._init_is_better(mode, min_delta)
+
+        if patience == 0:
+            self.is_better = lambda a, b: True
+
+    def step(self, metrics):
+        if self.best is None:
+            self.best = metrics
+            return False
+
+        if np.isnan(metrics):
+            log.info('Stopping early. Invalid metrics')
+            return True
+
+        if self.is_better(metrics, self.best):
+            self.num_bad_epochs = 0
+            self.best = metrics
+        else:
+            self.num_bad_epochs += 1
+
+        if self.num_bad_epochs >= self.patience:
+            log.info('Stopping early. Model did not improve in {} epochs'.format(self.patience))
+            return True
+
+        return False
+
+    def _init_is_better(self, mode, min_delta):
+        if mode not in {'min', 'max'}:
+            raise ValueError('mode ' + mode + ' is unknown!')
+        if mode == 'min':
+            self.is_better = lambda a, best: a < best - min_delta
+        if mode == 'max':
+            self.is_better = lambda a, best: a > best + min_delta
