86fb860576f0147bf7f37af13e965765b31c151a
Irynei Baran <i.baran@ucu.edu.ua>
2018-10-04 12:43:59 +0300

    Add folder with custom model architectures

---
+++ model/architectures/vgg_32x32.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+
+class VGG16_32x32(nn.Module):
+    """ VGG16 that works with 32x32 input (e.g. CIFAR10)"""
+    def __init__(self, num_classes):
+        super(VGG16_32x32, self).__init__()
+        self.layers = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+        self.features = self._make_layers(self.layers)
+        self.classifier = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
