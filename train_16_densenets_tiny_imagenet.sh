20868983a4d090a01976ae3cf150ca96ac54ba03
Irynei Baran <i.baran@ucu.edu.ua>
2018-10-31 16:52:24 +0200

    Add bash scripts for training several models

---
+++ train_16_densenets_tiny_imagenet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python train.py --config /configs/tiny_imagenet_image_classification/imgcl_densenet_tiny_imagenet_empty.json &&
+
+for i in {1..14}; do
+   python train.py --config /configs/tiny_imagenet_image_classification/imgcl_densenet_tiny_imagenet_$i.json
+done
